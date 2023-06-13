34b20118386bd3da5321a9146a7b02fbc0c0d330
NucleonGodX <racerpro41@gmail.com>
2023-06-13 13:35:11 +0530

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Footer(){
+    return(<h3 className="footer">@2023 #DreamLife</h3>)
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import Cars from "../images/cars.png"
 import { Link } from "react-router-dom";
 export default function Home(){
     return(<>
-        <img className="home-car" src={Cars}></img>
         <div className="home-major">
         <h1 className="home-heading">YOU GOT DREAMS, WE ARE HERE TO FULFILL</h1>
         <h3 className="home-info">We are here to fulfill your dreams of having a luxury experience with the luxury cars of your choice, So what are you waiting for rent the car of your dreams or host the car for other people!</h3>
