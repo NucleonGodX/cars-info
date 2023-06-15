f2509828533b4173131f9360af3149a9fb54d848
NucleonGodX <racerpro41@gmail.com>
2023-06-15 23:03:34 +0530

    Routes and Link added for nested host routes

--- src/components/HomeLayout.jsx
+++ src/components/HomeLayout.jsx
@@ -5,11 +5,13 @@ import { NavLink } from "react-router-dom";
 
 export default function HomeLayout(){
     return(<><nav className='host-nav'>
-    <NavLink className= "for-links" to="/host">Host</NavLink>
+    <NavLink className={({isActive})=>isActive?"after-links": "for-links"} to="/host" end>Dashboard</NavLink>
    
     <NavLink className={({isActive})=>isActive?"after-links": "for-links"} to= "/host/income">Income</NavLink>
    
+    <NavLink className={({isActive})=>isActive?"after-links": "for-links"} to="/host/cars">Cars</NavLink>
     <NavLink className={({isActive})=>isActive?"after-links": "for-links"} to="/host/reviews">Reviews</NavLink>
+
     </nav>
     <Outlet/></>
     
--- src/pages/Host/HostCars.jsx
+++ src/pages/Host/HostCars.jsx
@@ -1,4 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 export default function HostCars(){
-    return(<h1>Brotha</h1>)
+    const[hostCars, setHostCars]=React.useState([])
+    React.useEffect( ()=> {
+        fetch("/api/host/vans")
+        .then(res=>res.json())
+        .then(data=>setHostCars(data.vans)) },
+        [])
+    const hostCarElements=[hostCars.map(hostCar=>
+       <div className="host-container">
+        <Link to={hostCar.id} className="host-car-link">
+        <img className="host-image" src={hostCar.imageUrl}></img>
+        <div className="car-text">
+        <h3>{hostCar.name}</h3>
+        <p>${hostCar.price}/day</p></div></Link>
+       </div> )]
+
+       return(
+        <><h1 className="cars-heading">Your Listed Cars</h1>
+        {hostCarElements}</>
+       )
 }
