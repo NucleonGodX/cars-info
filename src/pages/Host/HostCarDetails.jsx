39e64025f39df4dd9ba1633729291769eab52e53
NucleonGodX <racerpro41@gmail.com>
2023-06-15 11:10:52 +0530

    further nesting of cars in host done

---
+++ src/pages/Host/HostCarDetails.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+export default function HostCarDetails(){
+    return(<h1>bruh</h1>)
+
+}
---
+++ src/pages/Host/HostCars.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function HostCars(){
+    return(<h1>Brotha</h1>)
+}
