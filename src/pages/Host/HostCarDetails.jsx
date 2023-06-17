befe326b1e546f526d33937eebc9487eb30174c6
NucleonGodX <racerpro41@gmail.com>
2023-06-17 09:31:14 +0530

    added back button using relative path

--- src/pages/Host/HostCarDetails.jsx
+++ src/pages/Host/HostCarDetails.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React from "react"
+import { useParams, Link } from "react-router-dom"
 export default function HostCarDetails(){
-    return(<h1>bruh</h1>)
+    const params=useParams()
+    const[hostCar, setHostCar]=React.useState([])
+    React.useEffect(()=>{
+        fetch( `/api/host/vans/${params.id}`)
+        .then(res=>res.json())
+        .then(data=>setHostCar(data.vans))
+    }
+    ,[])
+
+    return(
+        <section className="entire-car-container">
+            <Link to=".." relative="path" className="back-btn">--Back to Cars</Link>
+                <div className="host-car-detail">
+                    <img className="host-car-img" src={hostCar.imageUrl} />
+                    <div className="host-detail-text"> 
+                    <p className={hostCar.type}>{hostCar.type}</p>
+                   
+                        <h3>{hostCar.name}</h3>
+                        <h4>${hostCar.price}/day</h4>
+                    </div>
+                </div>
+        </section>
+    )
 
 }
