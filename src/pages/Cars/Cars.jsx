c5f9e51e557ae4e7cb78ae040f92e948b35a2755
NucleonGodX <racerpro41@gmail.com>
2023-06-21 23:40:13 +0530

    added search params

--- src/pages/Cars/Cars.jsx
+++ src/pages/Cars/Cars.jsx
@@ -1,13 +1,19 @@
 import React from "react"
-import {Link} from "react-router-dom"
+import {Link, useSearchParams} from "react-router-dom"
 export default function Cars(){
+    const[searchParams, setSearchParams]=useSearchParams()
     const[cars, setCars]=React.useState([])
+    const typeFilter=searchParams.get("type")
     React.useEffect( ()=> {
         fetch("/api/vans")
         .then(res=>res.json())
         .then(data=>setCars(data.vans)) },
         [])
-    const carElements=cars.map(car=><div className="cars-info"> 
+        const displayedCars=typeFilter
+        ?cars.filter(car=>car.type===typeFilter)
+        :cars
+
+    const carElements=displayedCars.map(car=><div className="cars-info"> 
         <Link to={`/cars/${car.id}`} className="box-element" style={{ textDecoration: 'none', color: 'black', fontSize: '16px' }}>
   <img className="cars" src={car.imageUrl} />
 
@@ -20,6 +26,12 @@ export default function Cars(){
      </div>)
     
     return(<>
-    <h1 className="heading">Explore Our Car Options</h1>
+
+    <h1 className="heading">Explore Our Car Options</h1>    
+    <div className="params-container"><Link className="single-param" to="?type=Ultimate-Luxury">Ultimate Luxury</Link>
+    <Link className="single-param" to="?type=Comfort">Comfort</Link>
+    <Link className="single-param" to="?type=Sports">Sports</Link>
+    <Link className="clear-param" to="">Clear All</Link></div>
+
     {carElements}</>)
 }
